b28da95850f38ee5d61c8b6a55a03dc5cf75a531
gtcronis <gabriel.tcronis@gmail.com>
2026-03-04 11:50:15 -0300

    Add files via upload

---
+++ Aula2Ex9.java
@@ -0,0 +1,11 @@
+//Gabriel Tolcsvai de Cronis
+//Análise e Desenvolvimento de Sistemas/ Senac/ 1º Semestre
+//Construir um algoritmo para apresentar: o cálculo da área de um quadrado de 350 metros de lado
+public class Aula2Ex9 {
+    public static void main(String[] args) {
+       double lado=350;
+       double area= lado*lado;
+       System.out.printf("A área do quadrado é: %.2f.m²%n", area);
+        
+    }
+}
