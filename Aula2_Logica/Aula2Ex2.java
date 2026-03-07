b2f19747de553474065364afebb9b889dee5439e
gtcronis <gabriel.tcronis@gmail.com>
2026-03-07 18:37:35 -0300

    Move arquivos da Aula2 para a pasta Aula2_Logica

---
+++ Aula2_Logica/Aula2Ex2.java
@@ -0,0 +1,17 @@
+
+     //Gabriel Tolcsvai de Cronis
+    //Análise e Desenvolvimento de Sistemas/ Senac/ 1º Semestre
+    //Faça um algoritmo para imprimir em cada linha: o seu nome completo, o seu Registro Acadêmico, o seu curso.
+
+
+        public class Aula2Ex2 {
+        public static void main(String[] args) {
+            String nome= "Gabriel Tolcsvai de Cronis";
+            String registroAcademico="RA 1440741084";
+            String curso="Análise e desenvolvimento de Sistemas";
+
+            System.out.println("Nome: "+ nome);
+            System.out.println("Registro Acadêmico: "+registroAcademico);
+            System.out.println("Curso: "+ curso);
+            }
+}
